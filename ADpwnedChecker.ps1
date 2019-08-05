ee4810e2e07f79da39b70a5e018dd0438b84eb95
LastLeigh <52006073+LastLeigh@users.noreply.github.com>
2019-08-05 10:58:21 -0500

    Delete ADpwnedChecker.ps1

--- ADpwnedChecker.ps1
+++
@@ -1,7 +0,0 @@
-Get-ADUser -Filter '*' -Properties * | $emailaddress = foreach-object -Property emailaddress 
-{
-$result = Get-PwnedAccount -emailaddress $emailaddress
-start-sleep -seconds 2
-$result | add-member -notepropertyname EmailAddress -NotePropertyValue $emailaddress 
-}
-| Tee-Object -Variable pwnedResults
