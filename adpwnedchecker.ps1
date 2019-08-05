0b84570a26235f8a05498fd25ae8a586c0ec0260
LastLeigh <52006073+LastLeigh@users.noreply.github.com>
2019-08-05 10:58:29 -0500

    Delete adpwnedchecker.ps1

--- adpwnedchecker.ps1
+++
@@ -1,8 +0,0 @@
-# Passing null "" to inside the foreach loop on this version. 
-get-aduser -filter * -Properties mail | select-object mail |
- ForEach-Object { # Loop over each user object.
-    $_.mail  # Grab the property
-    $result = Get-PwnedAccount -emailaddress $_ # Set $result to the result of the PWNed api module command running each object in $_
-    Start-Sleep -Seconds 2 # Avoid the API rate limiting.
-$result | Add-Member -NotePropertyName EmailAddress -NotePropertyValue $_ # Grab each entry made by this foreach-object loop
-} | Tee-Object -Variable pwnedResults
