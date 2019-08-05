8730eb428534e95f79d54564c6f1b5f9e065a2c0
LastLeigh <52006073+LastLeigh@users.noreply.github.com>
2019-08-05 10:57:13 -0500

    Add files via upload

---
+++ Get-PWNedAccounts.ps1
@@ -0,0 +1,26 @@
+﻿function Get-PWNedAccounts {
+    [CmdletBinding()]
+    param()   
+        # Building variables from grabing the AD Accounts from an AD.
+        $ADUsers = Get-ADUser -Filter '*' -Properties *
+        $EmailAddresses = $ADUsers.Emailaddress
+        # Beginning loop over the AD accounts to run them against the PWNed database.
+        foreach ($address in $EmailAddresses) {
+            # If statement to clean the output a bit.    
+            if ($address) {
+                $PwnedAddress = Get-PwnedAccount -EmailAddress $address
+                    # Further cleaning of the output so that only emails with results get listed
+                    if ($PwnedAddress.name -ne $null) {
+                        [PSCustomObject]@{
+                            EmailAddress = $address
+                            BadGuy = $PwnedAddress.Name
+                            BreachDate = $PwnedAddress.BreachDate
+                            Verified = $PwnedAddress.IsVerified
+                       }
+                }
+                
+                # Sleeping due to API limiting
+                Start-Sleep -s 2
+            }
+        }
+}
